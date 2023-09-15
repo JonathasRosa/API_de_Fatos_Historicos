9c5bb904c2e017a48be94380b93e13d3878d29e5
Jonathas Rosa <jonathasrosa85@gmail.com>
2023-09-15 13:14:54 -0300

    Update API

---
+++ service/service.js
@@ -0,0 +1,22 @@
+import fatosHistoricos from '../dados/fatos.js';
+
+function servicoValidaAno (ano) {
+    if(isNaN(ano)) {
+        return false;
+    } else {
+        if(ano >= 1920 && ano <= 2020) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+function servicoBuscarFatoPorAno (ano) {
+    let fatoSelecionado = fatosHistoricos.find(fato => {
+        return fato.Ano == ano;
+    });
+    return fatoSelecionado.Fato;
+}
+
+export {servicoBuscarFatoPorAno, servicoValidaAno};
